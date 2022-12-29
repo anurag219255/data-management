ca0b66e045672f0fa0fccf50e325feceb8736bbe
anurag219255 <singhanurag2194@gmail.com>
2022-12-29 08:28:45 -0500

    initial changes

--- app/app.py
+++ app/app.py
@@ -1,12 +1,44 @@
-#here we are integrating the read part and the write part
-
+# #here we are integrating the read part and the write part
+#
+#
+# import os
+# import sys
+#
+#
+# from read import get_json_reader
+#
+# from write import load_db_table
+#
+#
+# def process_table(BASE_DIR, conn, table_name):
+#     json_reader = get_json_reader(BASE_DIR, table_name)
+#     for df in json_reader:
+#         load_db_table(df, conn, table_name, df.columns[0])
+#
+#
+# def main():
+#     BASE_DIR = os.environ.get('BASE_DIR')
+#     table_names = sys.argv[1].split(',')#it will access first argument pass to the program so we are passing 1
+#     configs = dict(os.environ.items())
+#     conn = f'postgresql://{configs["DB_USER"]}:{configs["DB_PASS"]}@{configs["DB_HOST"]}:{configs["DB_PORT"]}/{configs["DB_NAME"]}'
+#
+#     for table_name in table_names:
+#         process_table(BASE_DIR, conn, table_name)
+#
+#
+#
+#
+#
+# if __name__=='__main__':
+#     main()
 
 import os
+import os
 import sys
-
 from app.read import get_json_reader
 from app.write import load_db_table
-
+# from read import dummy
+# import pandas as pd
 
 def process_table(BASE_DIR, conn, table_name):
     json_reader = get_json_reader(BASE_DIR, table_name)
@@ -14,18 +46,16 @@ def process_table(BASE_DIR, conn, table_name):
         load_db_table(df, conn, table_name, df.columns[0])
 
 
+
 def main():
     BASE_DIR = os.environ.get('BASE_DIR')
-    table_names = sys.argv[1].split(',')#it will access first argument pass to the program so we are passing 1
+    table_names = sys.argv[1].split(',')
     configs = dict(os.environ.items())
     conn = f'postgresql://{configs["DB_USER"]}:{configs["DB_PASS"]}@{configs["DB_HOST"]}:{configs["DB_PORT"]}/{configs["DB_NAME"]}'
-
     for table_name in table_names:
         process_table(BASE_DIR, conn, table_name)
 
 
-
-
-
-if __name__=='__main__':
+if __name__ == "__main__":
     main()
+
--- app/read.py
+++ app/read.py
@@ -1,17 +1,33 @@
+# import os
+# import pandas as pd
+#
+# #This function will read file path from different locations of the folder
+# def get_json_reader(BASE_DIR, table_name, chunksize=1000):
+#     file_name = os.listdir(f'{BASE_DIR}/{table_name}')[0]
+#     fp = f'{BASE_DIR}/{table_name}/{file_name}'
+#     return pd.read_json(fp, lines=True, chunksize=chunksize)
+#
+#
+# #Validate
+# if __name__ == "__main__":
+#     BASE_DIR = os.environ.get('BASE_DIR')
+#     table_name = os.environ.get('TABLE_NAME')
+#     json_reader = get_json_reader(BASE_DIR, table_name)
+#     for idx, df in enumerate(json_reader):
+#         print(f'Number of records in chunk with index {idx} is  {df.shape[0]}')
+
 import os
 import pandas as pd
 
-#This function will read file path from different locations of the folder
 def get_json_reader(BASE_DIR, table_name, chunksize=1000):
     file_name = os.listdir(f'{BASE_DIR}/{table_name}')[0]
     fp = f'{BASE_DIR}/{table_name}/{file_name}'
     return pd.read_json(fp, lines=True, chunksize=chunksize)
 
-
-#Validate
-if __name__ == "__main__":
+if __name__ == '__main__':
     BASE_DIR = os.environ.get('BASE_DIR')
     table_name = os.environ.get('TABLE_NAME')
-    json_reader = get_json_reader(BASE_DIR, table_name)
+    json_reader = get_json_reader(BASE_DIR,table_name)
     for idx, df in enumerate(json_reader):
-        print(f'Number of records in chunk with index {idx} is  {df.shape[0]}')
+        print(f'Number records in chunck with index {idx} is {df.shape[0]}')
+
